d399679ec7720b9f5b5e0099db0498496e224305
bloodyvlad <vlad@macsimum.com.ua>
2013-07-09 22:25:32 +0300

    Fixed identity field

--- catalog/model/tool/ulogin.php
+++ catalog/model/tool/ulogin.php
@@ -43,7 +43,7 @@ class ModelToolUlogin extends Model {
 		
 		$customer_group_info = $this->model_account_customer_group->getCustomerGroup($customer_group_id);
 		
-      	$this->db->query("INSERT INTO " . DB_PREFIX . "customer SET store_id = '" . (int)$this->config->get('config_store_id') . "', firstname = '" . $this->db->escape($data['firstname']) . "', lastname = '" . $this->db->escape($data['lastname']) . "', email = '" . $this->db->escape($data['email']) . "', telephone = '555555', fax = '555555', password = '" . $this->db->escape(md5($data['password'])) . "', newsletter = '0', customer_group_id = '" . (int)$customer_group_id . "', ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', status = '1', approved = '" . (int)!$customer_group_info['approval'] . "', date_added = NOW()");
+      	$this->db->query("INSERT INTO " . DB_PREFIX . "customer SET identity = '" . $this->db->escape($data['identity']) . "', store_id = '" . (int)$this->config->get('config_store_id') . "', firstname = '" . $this->db->escape($data['firstname']) . "', lastname = '" . $this->db->escape($data['lastname']) . "', email = '" . $this->db->escape($data['email']) . "', telephone = '555555', fax = '555555', password = '" . $this->db->escape(md5($data['password'])) . "', newsletter = '0', customer_group_id = '" . (int)$customer_group_id . "', ip = '" . $this->db->escape($this->request->server['REMOTE_ADDR']) . "', status = '1', approved = '" . (int)!$customer_group_info['approval'] . "', date_added = NOW()");
       	
 		$customer_id = $this->db->getLastId();
 		
